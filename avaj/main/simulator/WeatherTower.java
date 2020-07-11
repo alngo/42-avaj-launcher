46df368b332410aac37b92dd70848b81aaa8e84f
Alngo <alngo@student.42.fr>
2020-07-12 00:27:40 +0200

    Feat(WeatherTower): weatherTower + test.

--- avaj/main/simulator/WeatherTower.java
+++ avaj/main/simulator/WeatherTower.java
@@ -1,11 +1,17 @@
 package avaj.main.simulator;
 
+import avaj.main.aircraft.Coordinates;
+import avaj.main.weather.WeatherProvider;
+
 public class WeatherTower extends Tower {
-	public String getWeather() {
-		return null;
+	public String getWeather(Coordinates coordinates) {
+		WeatherProvider provider;
+
+		provider = WeatherProvider.getWeatherProvider();
+		return provider.getCurrentWeather(coordinates);
 	}
 
 	protected void changeWeather() {
-		System.out.println("Not implemented");
+		this.conditionsChanged();
 	}
 }
